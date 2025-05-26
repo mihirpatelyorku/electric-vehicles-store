c61d8ba3a754b8c3ce9877d6ad09f33950e5b61e
Mihir Patel <pmihir2001@gmail.com>
2025-05-26 18:58:29 -0400

    finish navbar component

--- elecric-vehicle-store/src/Components/NavbarEle.jsx
+++ elecric-vehicle-store/src/Components/NavbarEle.jsx
@@ -1,36 +1,58 @@
-import { Container } from "react-bootstrap";
-import { Navbar } from "react-bootstrap";
-import { Nav } from "react-bootstrap";
+import {
+  Container,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  Button,
+  Row,
+  Col,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { NavDropdown } from "react-bootstrap";
+
 function NavbarEle() {
   return (
-    <>
-      <Navbar bg="white" className="shadow-sm" data-bs-theme="light">
-        <Container>
-          <Nav.Link as={Link} to="/" className="fs-3 fw-bold text-dark">
-            ＬＵＥＶ
+    <Navbar bg="white" className="shadow-sm" data-bs-theme="light">
+      <Container>
+        <Nav.Link as={Link} to="/" className="fs-3 fw-bold text-dark">
+          ＬＵＥＶ
+        </Nav.Link>
+        <Nav className="mx-auto">
+          <Nav.Link as={Link} to="/Shop">
+            Shop Cars
           </Nav.Link>
-          <Nav className="mx-auto">
-            <Nav.Link as={Link} to="/Shop">
-              Shop Cars
-            </Nav.Link>
-            <NavDropdown title="Financing" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="./HowFinancingWorks">How Financing works</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} to="./CarCalculator">Car loan Calculator</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="More" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="./About">About us</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} to="./Contact">Contact us</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+          <NavDropdown title="Financing" id="basic-nav-dropdown">
+            <NavDropdown.Item as={Link} to="./HowFinancingWorks">
+              How Financing works
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item as={Link} to="./CarCalculator">
+              Car loan Calculator
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="More" id="basic-nav-dropdown">
+            <NavDropdown.Item as={Link} to="./About">
+              About us
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item as={Link} to="./Contact">
+              Contact us
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form className="d-flex align-items-center">
+          <Row className="gx-2">
+            <Col xs="auto">
+              <Form.Control type="text" placeholder="Search" className="me-2" />
+            </Col>
+            <Col xs="auto">
+              <Button type="submit">Submit</Button>
+            </Col>
+          </Row>
+        </Form>
+        <Button variant="success ms-3">Sign In / Register</Button>
+      </Container>
+    </Navbar>
   );
 }
 export default NavbarEle;
-
-
