7f202eff4818212ce94643bae329c2c6b1a0b23c
Mihir Patel <pmihir2001@gmail.com>
2025-06-06 21:25:59 -0400

    add cover for about page

--- elecric-vehicle-store/src/pages/About.jsx
+++ elecric-vehicle-store/src/pages/About.jsx
@@ -1,6 +1,21 @@
-function About(){
-    return(<>
-    about
-    </>);
+import cover from "../assets/aboutImg/AboutPageCover.jpg"
+function About() {
+  return (
+    <>
+      <div className="main">
+        <div className="sub-Main">
+          <h1>About LUEV</h1>
+          <p>
+            Welcome to LUEV, your trusted online destination for premium
+            electric vehicles. We are dedicated to transforming the way people
+            drive by offering cutting-edge electric cars that combine
+            innovation, sustainability, and performance.
+          </p>
+        </div>
+        <img src={cover} alt="" />
+      </div>
+      <div className="part-1"></div>
+    </>
+  );
 }
 export default About;
