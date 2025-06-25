b7ac78cd43d49e40c6cda65644dd80e2edd6bca9
Mihir Patel <pmihir2001@gmail.com>
2025-06-25 19:05:14 -0400

    fetch all cars from db

--- client(frontend)/src/pages/Shop.jsx
+++ client(frontend)/src/pages/Shop.jsx
@@ -1,5 +1,41 @@
-function Shop(){
-    return(<>
-    Shop</>)
+import { useEffect, useState } from "react";
+
+function Shop() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/cars`);
+        if (!res.ok) {
+          alert("failed to fetch data");
+        }
+        const data = await res.json();
+        setData(data);
+      } catch (error) {
+        alert(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
+      {data.map((item) => (
+        <div className="mt-5" key={item.id}>
+          <p>
+            <strong>Name:</strong>
+            {item.name}
+          </p>
+          <p>
+            <strong>Model:</strong>
+            {item.model}
+          </p>
+          <p>
+            <strong>Year:</strong>
+            {item.model_year}
+          </p>
+        </div>
+      ))}
+    </>
+  );
 }
 export default Shop;
